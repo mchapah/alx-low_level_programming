abc9aac282e15d50b0b39fba95113f9b0a3f1af2
Marian <marianchapa2@gmail.com>
2022-09-12 09:42:34 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: output to the standard error
+ *
+ * Return: 1 for success
+ */
+int main(void)
+{
+      fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);	
+	return (1);
+}
